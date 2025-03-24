022966476e0727027cf27f89290f545095f898a8
Moon <a11308@naver.com>
2025-03-24 07:42:18 +0000

    웹 서버 설치 자동화 적용

--- environments/dev/user_data.sh
+++ environments/dev/user_data.sh
@@ -3,3 +3,5 @@ sudo yum update -y
 sudo yum install -y nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
+
+echo "<h1>Welcome to My Terraform Web Server</h1>" | sudo tee /usr/share/nginx/html/index.html
