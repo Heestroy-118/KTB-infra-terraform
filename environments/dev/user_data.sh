bd73ee5d4a971de6e7f6d7e1f0b92288acbb47ee
Moon <a11308@naver.com>
2025-03-25 03:04:43 +0000

    ADD amazon-ssm-agent

--- environments/dev/user_data.sh
+++ environments/dev/user_data.sh
@@ -4,4 +4,9 @@ sudo yum install -y nginx
 sudo systemctl start nginx
 sudo systemctl enable nginx
 
+sudo yum install -y amazon-ssm-agent
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
+
+
 echo "<h1>Welcome to My Terraform Web Server</h1>" | sudo tee /usr/share/nginx/html/index.html
